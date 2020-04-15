804c8cb1552f825c755dddd713e17e50e5923e5d
buttnakedallday <rjwags72@gmail.com>
2020-04-15 14:43:19 -0500

    First commit

---
+++ Source/Excel_table_interp.py
@@ -0,0 +1,233 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLineEdit, QMessageBox
+from PyQt5.QtWidgets import QCheckBox, QLabel
+from Interpolation import int_table
+from PyQt5.QtCore import pyqtSlot, Qt
+#from pyqtspinner.spinner import WaitingSpinner as spin
+#import os.path
+
+#globals ex,
+
+#our widget
+class App(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        self.title = 'Interpolate a table from excel'
+        self.left = 10
+        self.top = 10
+        self.width = 700
+        self.height = 2000
+        self.initUI()
+    
+    def initUI(self):
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.left, self.top, self.width, self.height)
+        
+        #create textinput for input filepath box & title
+        self.file_path_label = QLabel(self)
+        self.file_path_label.setText('Input Filepath')
+        #self.file_path_label.resize(400,200)
+        self.file_path_label.move(10,15)
+        self.file_path = QLineEdit('Input Filepath', self)
+        self.file_path.setDragEnabled(True)
+        self.file_path.move(10, 40)
+        self.file_path.resize(500,32)
+        self.file_path.status = True
+        self.file_path.setDisabled(True)
+        self.file_path.textChanged.connect(self.set_out_to_in)
+        
+        #create textinput for input Sheet_name box & title
+        self.sheet_name_label = QLabel(self)
+        self.sheet_name_label.setText('Input Sheet Name')
+        #self.file_path_label.resize(400,200)
+        self.sheet_name_label.move(10,600)
+        self.sheet_name = QLineEdit('Sheet1', self)
+        self.sheet_name.setDragEnabled(True)
+        self.sheet_name.move(10, 620)
+        self.sheet_name.resize(500,32)
+        self.sheet_name.textChanged.connect(self.set_out_to_in_sheet)
+        
+        #create check box to decide wether to drop or type
+        self.input_type = QCheckBox("I would like to Drop in file",self)
+        self.input_type.setChecked(True)
+        self.input_type.stateChanged.connect(self.input_change)
+        self.input_type.move(10, 75)
+        
+        #create check box to to output file to same place
+        self.out_to_in = QCheckBox("Same as Input",self)
+        self.out_to_in.setChecked(True)
+        self.out_to_in.stateChanged.connect(self.same_out)
+        self.out_to_in.move(400, 680)
+        
+        #create textinput for output filepath box & title
+        self.out_file_path_label = QLabel(self)
+        self.out_file_path_label.setText('Output Filepath')
+        self.out_file_path_label.move(10,685)
+        self.out_file_path = QLineEdit(self.file_path.text(), self)
+        self.out_file_path.setDragEnabled(True)
+        self.out_file_path.move(10, 705)
+        self.out_file_path.resize(500,32)
+        self.out_file_path.status = True
+        self.out_file_path.setDisabled(True)
+        
+        #create textinput for output sheetname box & title ########
+        self.out_sheet_name_label = QLabel(self)
+        self.out_sheet_name_label.setText('Output Sheet Name')
+        self.out_sheet_name_label.move(10,740)
+        self.out_sheet_name = QLineEdit(self.sheet_name.text(), self)
+        self.out_sheet_name.setDragEnabled(True)
+        self.out_sheet_name.move(10, 765)
+        self.out_sheet_name.resize(500,32)
+        self.out_sheet_name.status = True
+        self.out_sheet_name.setDisabled(True)
+        
+        #create check box  to output sheet to same sheetname
+        self.out_sheet = QCheckBox("Same as Input",self)
+        self.out_sheet.setChecked(True)
+        self.out_sheet.stateChanged.connect(self.same_out_sheet)
+        self.out_sheet.move(400, 740)
+        
+        #create step size input
+        self.step_label = QLabel(self)
+        self.step_label.setText('Step Size:')
+        self.step_label.move(390,815)
+        self.step_label.setStyleSheet("font: 23")
+        self.step = QLineEdit('0.1', self)
+        self.step.setDragEnabled(True)
+        self.step.move(460, 810)
+        self.step.resize(50,32)
+        
+        #create column to interpolate from input
+        self.col_label = QLabel(self)
+        self.col_label.setText('Column:')
+        self.col_label.move(200,815)
+        self.col_label.setStyleSheet("font: 23")
+        self.col = QLineEdit(None, self)
+        self.col.setDragEnabled(True)
+        self.col.move(260, 810)
+        self.col.resize(50,32)
+        self.col.setDisabled(True)
+        self.col.status = True
+        
+        #create check box to decide wether to drop or type
+        self.col_box = QCheckBox("interpolate from first column with data",self)
+        self.col_box.setChecked(True)
+        self.col_box.stateChanged.connect(self.ret_none)
+        self.col_box.move(250, 75)
+        
+        
+        # create button
+        self.updt_butt = QPushButton('Interpolate Table', self)
+        self.updt_butt.move(10,810)
+        self.updt_butt.clicked.connect(self.on_click)
+        
+        #initalize drop area
+        self.drop_area = droparea('Drop here.', self)
+        self.drop_area.resize(500, 500)
+        self.drop_area.move(10,100)
+        self.drop_area.status = False
+        self.drop_area.setDisabled(False)
+        
+        #remove file from drop area
+        
+        
+        self.show()
+        
+    @pyqtSlot()
+    def on_click(self):
+        msgBox = QMessageBox()
+        msgBox.setText('Interpolating')
+        msgBox.setWindowTitle("Status")
+        msgBox.setIcon(QMessageBox.Information)
+        msgBox.show()
+        file_path = self.file_path.text()
+        sheet_name_read = self.sheet_name.text()
+        steps = self.step.text()
+        steps = float(steps)
+        col_to_int = self.col.text()
+        same_file = self.out_to_in.isChecked()
+        same_sheet = self.out_sheet.isChecked()
+        file_path_out = self.out_file_path.text()
+        sheet_name_write = self.out_sheet_name.text()
+        status = int_table(file_path, sheet_name_read, steps, col_to_int,
+                             file_path_out, sheet_name_write, same_sheet, same_file)
+
+        msgBox.setText(str(status))
+        msgBox.setStandardButtons(QMessageBox.Ok)
+        msgBox.show()
+        msgBox.exec_()
+        
+#int_table(fp,sn,steps,col_to_interp_from,fp2,sn2,same_sheet,same_file)
+        
+    #function to change the file input area
+    def input_change(self):
+        self.file_path.status = not(self.file_path.status)
+        self.drop_area.status = not(self.drop_area.status)
+        self.drop_area.setDisabled(self.drop_area.status)
+        self.file_path.setDisabled(self.file_path.status)
+    
+    #function to set inout file to output file
+    def same_out(self):
+        self.out_file_path.status = not(self.out_file_path.status)
+        self.out_file_path.setDisabled(self.out_file_path.status)
+        if(self.out_file_path.status == True):
+            self.out_file_path.setText(self.file_path.text())
+        else:
+            self.out_file_path.setText('Output Filepath')
+    
+    def set_out_to_in(self):
+        if(self.out_to_in.isChecked() == True):
+            self.out_file_path.setText(self.file_path.text())
+    
+    def set_out_to_in_sheet(self):
+        if(self.out_sheet.isChecked() == True):
+            self.out_sheet_name.setText(self.sheet_name.text())
+            
+    def same_out_sheet(self):
+        self.out_sheet_name.status = not(self.out_sheet_name.status)
+        self.out_sheet_name.setDisabled(self.out_sheet_name.status)
+        if(self.out_sheet_name.status == True):
+            self.out_sheet_name.setText(self.sheet_name.text())
+        else:
+            self.out_sheet_name.setText('Output Sheet Name')
+            
+    def ret_none(self):
+        self.col.status = not(self.col.status)
+        self.col.setDisabled(self.col.status)
+        if(self.col.status == True):
+            self.col.setText(None)
+        
+    #drop area class
+class droparea(QLabel):
+    
+    def __init__(self, title, parent):
+        super().__init__(title, parent)
+        self.setAcceptDrops(True)
+        self.setAlignment(Qt.AlignCenter)
+        self.setStyleSheet("border: 1px solid black;") 
+    def dragEnterEvent(self, e):
+        e.accept()
+        #global file_path_in
+        #self.file_path.setText(e)
+# =============================================================================
+#         if e.mimeData().hasFormat(csvDATA):
+#             e.accept()
+#         else:
+#             e.ignore()
+# =============================================================================
+
+    def dropEvent(self, e):
+        self.setText(e.mimeData().text())
+        global ex
+        txt = str(e.mimeData().text())
+        txt = txt.strip('file:///')
+        ex.file_path.setText(txt)
+
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = App()
+    sys.exit(app.exec_())
+
---
+++ Source/Interpolation.py
@@ -0,0 +1,235 @@
+#from tqdm import tqdm
+import numpy as np
+import pandas as pd
+import openpyxl
+import string
+
+# =============================================================================
+# # required inpuuts 
+# fp = 'C:/Users/rjw3/OneDrive/Desktop/Test.xlsx'
+# sn = 'Sheet1'
+# steps =.001
+# col_to_interp_from = None
+# 
+# 
+# #### check for optional user input
+# fp2 = str()
+# sn2 = str()
+# same_sheet = True
+# same_file = True
+# =============================================================================
+
+
+def interp(x1,y1, x3,y3, x2):
+    top = (x2 - x1) * (y3 - y1)
+    bot = (x3 - x1)
+    y2 = (top/bot) + y1
+    return y2
+
+#inatlize
+first_row = int()
+first_col = str()
+
+#### add col to interplolate
+
+#return true if input is NaN
+def isNaN(num):
+    return (num!= num) or (num == None)
+
+#deleta all NaN
+def find_table(DataFrame):
+    #check for empty rows
+    row_found = False
+    col_found = False
+    for i in (DataFrame.index.values):
+        test_arr = np.array([])
+        for o in DataFrame.columns.values:
+            test_arr = np.append(test_arr,
+                                 isNaN(DataFrame.loc[i, o]))
+        if(np.sum(test_arr) == np.size(test_arr)):
+            DataFrame = DataFrame.drop(i)
+        else:
+            if(row_found==False):
+                first_row = i + 1 
+                #print('\n' + 'first row =' + str(first_row))
+                row_found = True
+    #check for empty columns
+    for i in (DataFrame.columns.values):
+        test_arr = np.array([])
+        for o in DataFrame.index.values:
+            test_arr = np.append(test_arr,
+                                 isNaN(DataFrame.loc[o, i]))
+        if(np.sum(test_arr) == np.size(test_arr)):
+            DataFrame = DataFrame.drop(i, axis=1)
+        else:
+            if(col_found==False):
+                first_col = i
+                col_found = True
+    found=True
+    try:
+        return DataFrame, first_row, first_col, found
+    
+    except UnboundLocalError:
+        found=False
+        first_row = None
+        first_col = None
+        return DataFrame, first_row, first_col, found
+
+#set anything that is not a float or int to NaN
+def NaN_non_values(Dataframe):
+    for i in (Dataframe.columns.values):
+        for o in Dataframe.index.values:
+            y = Dataframe.loc[o,i]
+            if((isinstance(y,float) or isinstance(y, int)) != True):
+                Dataframe.loc[o,i] = None
+    return Dataframe
+
+
+
+#create dictionary for excel columns
+alph_dic = dict()
+L = np.arange(0,25)
+numb = 0
+st = str()
+while(numb<10000):
+    for i in range(0,25):
+        for o in L:
+            alph_dic[numb] = str(st) + string.ascii_uppercase[o]
+            numb = numb + 1
+        st = str(st) + str(string.ascii_uppercase[i])
+
+
+# rename columns with the appropriate excel keys
+def rename_col(DataFrame):
+    dic = dict()
+    for i in DataFrame.columns.values:
+        key = alph_dic[i]
+        dic[i] = key
+    DataFrame = DataFrame.rename(columns=dic)
+    return DataFrame
+
+
+def int_table(fp, sn, steps, col_to_interp_from, fp2, sn2, same_sheet, same_file):
+    
+    #pull initial table
+    try:
+        table = pd.read_excel(fp, sheet_name=sn, header=None)
+    except PermissionError:
+            return 'Close the file & try again'
+    table = rename_col(table)
+    table = NaN_non_values(table)
+    table, first_row, first_col, Data_found = find_table(table)
+    if(Data_found==False):
+        return 'No data Found'
+    # interpolate and create new table
+    n_table = pd.DataFrame(columns=table.columns.values)
+    current_row = first_row
+    if((col_to_interp_from==None) or(col_to_interp_from=='')):
+        col = first_col
+    else:
+        col = col_to_interp_from
+    
+    #check for appropriate step size
+    bol_test = True
+
+    for i in (table.index.values):
+        if(i < len(table.index.values)):
+            test1 = abs((table.loc[i, col] - table.loc[i+1, col])) > \
+            abs((table.loc[i, col] + steps) - table.loc[i+1, col])
+        if(test1==False):
+            bol_test = False
+    if(bol_test==False):
+        return 'Inappropriate step size'
+    
+    #interpolate
+
+    for i in (table.index.values):
+        x1 = table.loc[i,col]
+        x2 = x1 + steps
+        if(i == first_row-1):
+            x3 = table.loc[i + 1,col]
+            n_table.loc[current_row, :] = table.loc[i, :]
+        else:
+            
+            if(i < ((len(table.index.values)-2) + first_row)):
+                current_row = current_row + 1
+                x3 = table.loc[i + 1,col]
+                n_table.loc[current_row, :] = table.loc[i, :]
+            else:
+                x3 = x2
+                current_row = current_row + 1
+                n_table.loc[current_row, :] = table.loc[i, :]
+        while(abs(round(x2 - x3,5))>0):
+            current_row = current_row + 1
+            n_table.loc[current_row , col] = x2
+            for o in table.columns.values:
+                if(o!=col):
+                    y1 = table.loc[i, o]
+                    y3 = table.loc[i + 1, o]
+                    y2 = interp(x1,y1, x3,y3, x2)
+                    n_table.loc[current_row, o] = y2
+
+            x2 = x2 + steps
+    
+    if(same_file==True):
+        wb = openpyxl.load_workbook(fp)
+        if(same_sheet==True):
+            work_sheet = wb[sn]
+            for i in n_table.columns.values:
+                col = str(i)
+                for o in n_table.index.values:
+                    row = str(o)
+                    cell = col + row
+                    work_sheet[cell] = n_table.loc[o, i]
+
+            wb.save(fp)
+            
+        else:
+            try:
+                work_sheet = wb[sn2]
+            except KeyError:
+                wb.create_sheet(sn2)
+                work_sheet = wb[sn2]
+            for i in (n_table.columns.values):
+                col = str(i)
+                for o in n_table.index.values:
+                    row = str(o)
+                    cell = col + row
+                    work_sheet[cell] = n_table.loc[o, i]
+
+            wb.save(fp)
+            
+    else:
+        try:
+            wb = openpyxl.load_workbook(fp2)
+        except FileNotFoundError:
+            wb = openpyxl.Workbook()
+        if(same_sheet==True):
+            wb.create_sheet(sn)
+            work_sheet = wb[sn]
+            for i in (n_table.columns.values):
+                col = str(i)
+                for o in n_table.index.values:
+                    row = str(o)
+                    cell = col + row
+                    work_sheet[cell] = n_table.loc[o, i]
+
+            wb.save(fp2)
+            
+        else:
+            try:
+                work_sheet = wb[sn2]
+            except KeyError:
+                wb.create_sheet(sn2)
+                work_sheet = wb[sn2]
+            for i in (n_table.columns.values):
+                col = str(i)
+                for o in n_table.index.values:
+                    row = str(o)
+                    cell = col + row
+                    work_sheet[cell] = n_table.loc[o, i]
+            wb.save(fp2)
+    
+   
+    return 'Interpolation complete'
+
